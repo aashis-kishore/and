ce6ebbcae2a1886b7c2cf89cd3195b9f72b5d456
Aashis Kishore <aashiskishore@gmail.com>
2019-06-09 15:17:51 +0530

    Add provision to remove generated dirs from root dir

--- premake5.lua
+++ premake5.lua
@@ -1,2 +1,12 @@
 workspace "datum_algum"
     configurations { "debug", "release" }
+
+
+newaction {
+    trigger = "clean",
+    description = "remove all generated directories from the root directory",
+    execute = function()
+        os.rmdir("./obj")
+        os.rmdir("./build")
+    end,
+}
