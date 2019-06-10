d46454d24c2d29a1791c75c643d50cc59eb7fff5
Aashis Kishore <aashiskishore@gmail.com>
2019-06-10 17:39:59 +0530

    Make function prototypes visibe only in DEBUG mode

--- dev_support/print_messages/prntmsgs.h
+++ dev_support/print_messages/prntmsgs.h
@@ -11,12 +11,12 @@
     #define DTALGM_PRINT(msg) printf("%s\n", msg);
     #define DTALGM_PRINT_ERR(func_name, err_msg) printf("[ERROR] : [%s] => %s.\n", func_name, err_msg);
     #define DTALGM_PRINT_WARN(func_name, warn_msg) printf("[WARNING] : [%s] => %s.\n", func_name, warn_msg);
+
+    void dtalgm_prnt_arr_as_byts(void*, size_t);
 #else
 
     #define DTALGM_PRINT(msg)
     #define DTALGM_PRINT_ERR(func_name, err_msg)
     #define DTALGM_PRINT_WARN(func_name, warn_msg)
 #endif
-
-    void dtalgm_prnt_arr_as_byts(void*, size_t);
 #endif
