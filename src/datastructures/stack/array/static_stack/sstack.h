c60b000d0ab2e8c3f08c76126b70753f41adedfd
Aashis Kishore <aashiskishore@gmail.com>
2019-06-12 17:32:18 +0530

    Use new naming scheme

--- src/datastructures/stack/array/static_stack/sstack.h
+++ src/datastructures/stack/array/static_stack/sstack.h
@@ -8,15 +8,15 @@
 
 typedef struct sstack* sstackptr_t;
 
-#define SSTK_DEFAULT_SIZE ((size_t)32)
+#define SS_DEFAULT_SIZE ((size_t)32)
 
-sstackptr_t sstk_create(size_t, size_t);
-int8_t sstk_isEmpty(sstackptr_t);
-int8_t sstk_isFull(sstackptr_t);
-int8_t sstk_push(sstackptr_t, void*, size_t);
-void* sstk_pop(sstackptr_t, AND_BOOL);
-void* sstk_peek(sstackptr_t, size_t);
-void* sstk_swap(sstackptr_t);
-int8_t sstk_destroy(sstackptr_t, AND_BOOL);
+sstackptr_t ss_create(size_t, size_t);
+int8_t ss_isEmpty(sstackptr_t);
+int8_t ss_isFull(sstackptr_t);
+int8_t ss_push(sstackptr_t, void*, size_t);
+void* ss_pop(sstackptr_t, AND_BOOL);
+void* ss_peek(sstackptr_t, size_t);
+void* ss_swap(sstackptr_t);
+int8_t ss_destroy(sstackptr_t, AND_BOOL);
 
 #endif
