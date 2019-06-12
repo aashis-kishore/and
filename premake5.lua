be0710d3f41b3d11bf5710b614a51e02a16311f2
Aashis Kishore <aashiskishore@gmail.com>
2019-06-12 17:16:27 +0530

    Reflect change in path to file

--- premake5.lua
+++ premake5.lua
@@ -9,7 +9,7 @@ dofile("./tests/dev_support/test_print_messages/test_print_messages.lua")
 
 
 -- src projects
-dofile("./src/datastructures/stack/c/array/static_stack/static_stack.lua")
+dofile("./src/datastructures/stack/array/static_stack/static_stack.lua")
 dofile("./src/datastructures/array/dynamic_array/dynamic_array.lua")                  -- dynamic_array
 
 -- src projects tests
