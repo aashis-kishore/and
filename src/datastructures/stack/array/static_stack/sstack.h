232e03564f0b691d2c21105da36b885093d83a80
Aashis Kishore <aashiskishore@gmail.com>
2019-06-12 17:29:33 +0530

    Remove unnecessary macros and enums

--- src/datastructures/stack/array/static_stack/sstack.h
+++ src/datastructures/stack/array/static_stack/sstack.h
@@ -4,21 +4,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include "and.h"
 
 typedef struct sstack* sstackptr_t;
 
 #define SSTK_DEFAULT_SIZE ((size_t)32)
-
-#define SSTK_OK ((int8_t)0)
-#define SSTK_NOK ((int8_t)-1)
-#define SSTK_PNOK ((void*)0)
-
-typedef enum {
-    SSTK_FALSE = 0,
-    SSTK_TRUE
-} SSTK_BOOL;
-
-#define sstk_getElement(data, type) *((type*)data)
 
 sstackptr_t sstk_create(size_t, size_t);
 int8_t sstk_isEmpty(sstackptr_t);
