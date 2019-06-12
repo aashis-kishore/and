1eae1bb91a703938ce329915f55557d181b37ee9
Aashis Kishore <aashiskishore@gmail.com>
2019-06-12 23:07:21 +0530

    Add test for 'destroy'

--- tests/src/test_static_array/test_static_array.c
+++ tests/src/test_static_array/test_static_array.c
@@ -56,10 +56,67 @@ Suite* sa_create_suite(void) {
 // ==================================================================================================
 
 
+// ========================================================================================= [sa_destroy(sarrayptr_t, AND_BOOL)]
+// test: sa_destroy(NULL, AND_FALSE)
+START_TEST(test_sa_destroy__args__NULL__AND_FALSE) {
+    int8_t destroy_stat = sa_destroy(NULL, AND_FALSE);
+    ck_assert_int_eq(destroy_stat, AND_NOK);
+} END_TEST
+
+// test: sa_destroy(NULL, AND_TRUE)
+START_TEST(test_sa_destroy__args__NULL__AND_TRUE) {
+    int8_t destroy_stat = sa_destroy(NULL, AND_TRUE);
+    ck_assert_int_eq(destroy_stat, AND_NOK);
+} END_TEST
+   
+// test: sa_destroy(sarr, AND_FALSE)
+START_TEST(test_sa_destroy__args__sarr__AND_FALSE) {
+    sarrayptr_t sarr = ss_create(0, sizeof(int));
+
+    int8_t destroy_stat = sa_destroy(sarr, AND_FALSE);
+    ck_assert_int_eq(destroy_stat, AND_OK);
+} END_TEST
+
+// test: sa_destroy(sarr, AND_TRUE)
+START_TEST(test_sa_destroy__args__sarr__AND_TRUE) {
+    sarrayptr_t sarr = ss_create(0, sizeof(char*));
+
+    char elmnt[] = "test string two";
+    sa_insert(sarr, elmnt, 2, strlen(elmnt)+1);
+
+    int8_t destroy_stat = sa_destroy(sarr, AND_TRUE);
+    ck_assert_int_eq(destroy_stat, AND_OK);
+} END_TEST
+
+
+Suite* sa_destroy_suite(void) {
+    Suite* suite;
+    TCase *tc_failure, *tc_success;
+
+    suite = suite_create("Destroy");
+
+    tc_failure = tcase_create("Failure");
+    tcase_add_test(tc_failure, test_sa_destroy__args__NULL__AND_FALSE);
+    tcase_add_test(tc_failure, test_sa_destroy__args__NULL__AND_TRUE);
+
+    tc_success = tcase_create("Success");
+    tcase_add_test(tc_success, test_sa_destroy__args__sarr__AND_FALSE);
+    tcase_add_test(tc_success, test_sa_destroy__args__sarr__AND_TRUE);
+
+    suite_add_tcase(suite, tc_failure);
+    suite_add_tcase(suite, tc_success);
+
+    return suite;
+}
+
+// =========================================================================================
+
+
 int main(int argc, char** argv) {
     int num_tests_failed;
     
     SRunner* suite_runner = srunner_create(sa_create_suite());
+    srunner_add_suite(suite_runner, sa_destroy_suite());
 
     if (argc == 1) {
         srunner_run_all(suite_runner, CK_NORMAL);
