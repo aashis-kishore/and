c088a6387f0bab5a16f16dee69f5c192e3113937
Aashis Kishore <aashiskishore@gmail.com>
2019-06-18 22:49:38 +0530

    Add macros for pretty printing

--- src/and/viewmemutils/viewmemutils.h
+++ src/and/viewmemutils/viewmemutils.h
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define VMU_RESET "\e[0m"
+#define VMU_RED "\e[1;91m"
+#define VMU_GREEN "\e[1;32m"
+#define VMU_YELLOW "\e[1;93m"
+#define VMU_BLUE "\e[1;36m"
+#define VMU_WHITE "\e[1;89m"
+#define VMU_GREY "\e[1;90m"
 
 typedef enum {
     VMU_BYTE = 8,
