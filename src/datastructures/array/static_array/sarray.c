20e61660e2454138315b7acacfe79e0521006fd1
Aashis Kishore <aashiskishore@gmail.com>
2019-06-19 23:56:15 +0530

    Change attribute name to reflect its purpose

--- src/datastructures/array/static_array/sarray.c
+++ src/datastructures/array/static_array/sarray.c
@@ -4,18 +4,18 @@
 typedef struct sarray {
     void* buffer;
     bitvectorptr_t bv;
-    size_t num_elements;
+    size_t max_num_elements;
     size_t element_size;
 } sarray_t;
 
 
-sarrayptr_t sa_create(size_t num_elements, size_t element_size) {
+sarrayptr_t sa_create(size_t max_num_elements, size_t element_size) {
     if (!element_size) {
         AND_PRINT_ERR("sa_create", "Element size cannot be 0")
         return AND_PNOK;
     }
     
-    num_elements =  (num_elements) ? num_elements : SA_DEFAULT_SIZE; 
+    max_num_elements =  (max_num_elements) ? max_num_elements : SA_DEFAULT_SIZE; 
 
     sarrayptr_t sarr = malloc(sizeof(sarray_t));
 
@@ -24,21 +24,21 @@ sarrayptr_t sa_create(size_t num_elements, size_t element_size) {
         return AND_PNOK;
     }
 
-    sarr->buffer = malloc(num_elements*element_size);
+    sarr->buffer = malloc(max_num_elements*element_size);
 
     if (!sarr->buffer) {
         AND_PRINT_ERR("sa_create", "Unable to allocate memory for array buffer")
         return AND_PNOK;
     }
 
-    sarr->bv = bv_create(num_elements, BV_DEFAULT_GROWTH_FACTOR, BV_DEFAULT_LOAD_FACTOR, AND_FALSE);
+    sarr->bv = bv_create(max_num_elements, BV_DEFAULT_GROWTH_FACTOR, BV_DEFAULT_LOAD_FACTOR, AND_FALSE);
 
     if (!sarr->bv) {
         AND_PRINT_ERR("sa_create", "Unable to create auxiliaries => (bitvector)")
         return AND_PNOK;
     }
 
-    sarr->num_elements = num_elements;
+    sarr->max_num_elements = max_num_elements;
     sarr->element_size = element_size;
 
     return sarr;
@@ -50,7 +50,7 @@ int8_t sa_insert(sarrayptr_t sarr, void* element, size_t index, size_t element_s
         return AND_NOK;
     }
 
-    if (index >= sarr->num_elements) {
+    if (index >= sarr->max_num_elements) {
         AND_PRINT_ERR("sa_insert", "Index out of bounds")
         return AND_NOK;
     }
@@ -97,7 +97,7 @@ void* sa_get(sarrayptr_t sarr, size_t index) {
         return AND_PNOK;
     }
 
-    if (index >= sarr->num_elements) {
+    if (index >= sarr->max_num_elements) {
         AND_PRINT_ERR("sa_get", "Index out of bounds")
         return AND_PNOK;
     }
@@ -114,7 +114,7 @@ int8_t sa_destroy(sarrayptr_t sarr, AND_BOOL has_mem_alloced_element) {
     }
 
     if (has_mem_alloced_element) {
-        for (size_t i=0; i < sarr->num_elements; i++) {
+        for (size_t i=0; i < sarr->max_num_elements; i++) {
             int8_t status = bv_isBitSet(sarr->bv, i);
             if (status != AND_NOK && status == AND_TRUE) {
                 void* currnt_elmnt = (void*)(*((uintptr_t*)sarr->buffer + i));
