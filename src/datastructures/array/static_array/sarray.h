8cbdb61047683359404a8a7d2528b80b1881e603
Aashis Kishore <aashiskishore@gmail.com>
2019-06-20 15:01:42 +0530

    Extend interface

--- src/datastructures/array/static_array/sarray.h
+++ src/datastructures/array/static_array/sarray.h
@@ -9,8 +9,12 @@ typedef struct sarray* sarrayptr_t;
 #define SA_DEFAULT_SIZE ((size_t)32)
 
 sarrayptr_t sa_create(size_t, size_t);
+size_t sa_getMaxNumElements(sarrayptr_t, int8_t*);
+size_t sa_getElementSize(sarrayptr_t, int8_t*);
 int8_t sa_insert(sarrayptr_t, void*, size_t, size_t);
+void* sa_delete(sarrayptr_t, size_t);
 void* sa_get(sarrayptr_t, size_t);
+void* sa_swap(sarrayptr_t, size_t, size_t);
 int8_t sa_destroy(sarrayptr_t, AND_BOOL);
 
 #endif
