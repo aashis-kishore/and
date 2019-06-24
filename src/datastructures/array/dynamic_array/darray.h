85f8c9e87ef7d550b98a3047fcf63854a2cd7119
Aashis Kishore <aashiskishore@gmail.com>
2019-06-24 10:12:37 +0530

    Declare interface of 'dynamic_array'

---
+++ src/datastructures/array/dynamic_array/darray.h
@@ -0,0 +1,22 @@
+#ifndef _DARRAY_H
+#define _DARRAY_H
+
+#include "and.h"
+#include "bitvector.h"
+
+typedef struct darray* darrayptr_t;
+
+#define DA_DEFAULT_SIZE ((size_t)32)
+
+darrayptr_t da_create(size_t, size_t, uint8_t, uint8_t);
+size_t da_getMaxNumElements(darrayptr_t, int8_t*);
+size_t da_getElementSize(darrayptr_t, int8_t*);
+size_t da_getGrowthFactor(darrayptr_t, int8_t*);
+size_t da_getLoadFactor(darrayptr_t, int8_t*);
+int8_t da_insert(darrayptr_t, void*, size_t, size_t);
+void* da_delete(darrayptr_t, size_t);
+void* da_get(darrayptr_t, size_t);
+void* da_swap(darrayptr_t, size_t, size_t);
+int8_t da_destroy(darrayptr_t, AND_BOOL);
+
+#endif
