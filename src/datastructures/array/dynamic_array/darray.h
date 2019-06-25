1a22678f9709d7c024a69a8366da59801b2cc12e
Aashis Kishore <aashiskishore@gmail.com>
2019-06-25 10:01:11 +0530

    Add limit defining macros

--- src/datastructures/array/dynamic_array/darray.h
+++ src/datastructures/array/dynamic_array/darray.h
@@ -7,8 +7,20 @@
 typedef struct darray* darrayptr_t;
 
 #define DA_DEFAULT_SIZE ((size_t)32)
-#define DA_DEFAULT_GROWTH_FACTOR ((uint8_t)150)
-#define DA_DEFAULT_LOAD_FACTOR ((uint8_t)75)
+
+#define DA_MIN_GROWTH_FACTOR ((uint8_t)125)
+#define DA_MAX_GROWTH_FACTOR ((uint8_t)200)
+
+#ifndef DA_DEFAULT_GROWTH_FACTOR
+    #define DA_DEFAULT_GROWTH_FACTOR ((uint8_t)150)
+#endif
+
+#define DA_MIN_LOAD_FACTOR ((uint8_t)25)
+#define DA_MAX_LOAD_FACTOR ((uint8_t)100)
+
+#ifndef DA_DEFAULT_LOAD_FACTOR
+    #define DA_DEFAULT_LOAD_FACTOR ((uint8_t)75)
+#endif
 
 darrayptr_t da_create(size_t, size_t, uint8_t, uint8_t);
 size_t da_getMaxNumElements(darrayptr_t, int8_t*);
