924368a0ab7f25a5754e12187a28cfc5e4912311
Aashis Kishore <aashiskishore@gmail.com>
2019-07-04 11:59:14 +0530

    Add tests for function 'fibRec'

--- tests/test_fibonacci/test_fibonacci.c
+++ tests/test_fibonacci/test_fibonacci.c
@@ -50,6 +50,46 @@ Suite* fibIter_suite(void) {
 
 
 // ================================================================================================== [fibRec(size_t, size_t)]
+// test: fibRec(4, 0)
+START_TEST(test_fibRec__args__4__0) {
+    ck_assert_int_eq(fibRec(4, 0), AND_NOK);
+} END_TEST
+
+// test: fibRec(0, 0)
+START_TEST(test_fibRec__args__0__0) {
+    ck_assert_int_eq(fibRec(0, 0), AND_OK);
+} END_TEST
+
+// test: fibRec(0, 15)
+START_TEST(test_fibRec__args__0__15) {
+    ck_assert_int_eq(fibRec(0, 15), AND_OK);
+} END_TEST
+
+// test: fibRec(10, 20)
+START_TEST(test_fibRec__args__10__20) {
+    ck_assert_int_eq(fibRec(10, 20), AND_OK);
+} END_TEST
+
+
+Suite* fibRec_suite(void) {
+    Suite* suite;
+    TCase *tc_failure, *tc_success;
+
+    suite = suite_create("FibRec");
+
+    tc_failure = tcase_create("Failure");
+    tcase_add_test(tc_failure, test_fibRec__args__4__0);
+
+    tc_success = tcase_create("Success");
+    tcase_add_test(tc_failure, test_fibRec__args__0__0);
+    tcase_add_test(tc_failure, test_fibRec__args__0__15);
+    tcase_add_test(tc_failure, test_fibRec__args__10__20);
+
+    suite_add_tcase(suite, tc_failure);
+    suite_add_tcase(suite, tc_success);
+
+    return suite;
+}
 
 // ==================================================================================================
 
@@ -68,6 +108,7 @@ int main(int argc, char** argv) {
     int num_tests_failed;
     
     SRunner* suite_runner = srunner_create(fibIter_suite());
+    srunner_add_suite(suite_runner, fibRec_suite());
 
     if (argc == 1) {
         srunner_run_all(suite_runner, CK_NORMAL);
