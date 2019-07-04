36b9cb873877f9195474da33633357bc46a892a2
Aashis Kishore <aashiskishore@gmail.com>
2019-07-04 11:21:42 +0530

    Declare interface

---
+++ src/algorithms/other/fibonacci/fibonacci.h
@@ -0,0 +1,11 @@
+#ifndef _FIBONACCI_H
+#define _FIBONACCI_H
+
+#include "and.h"
+
+int8_t fibIter(size_t, size_t);
+int8_t fibRec(size_t, size_t);
+int8_t fibTab(size_t, size_t);
+int8_t fibMem(size_t, size_t);
+
+#endif
